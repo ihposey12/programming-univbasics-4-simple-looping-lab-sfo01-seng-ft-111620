ed8df1ec4b2b2329d95aaeac1c4585248f60074d
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-18 02:29:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,10 +19,4 @@ def loop_message_n_times(string, integer)
 end
 
 def output_array(array)
-  counter = 0
-  
-  while counter < array do
-    puts array[counter]
-    counter += 1
-  end
-end
+  array.each {|i| 
